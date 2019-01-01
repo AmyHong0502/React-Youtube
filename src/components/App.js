71a657c17823eef75e5b6a085e1b882bdc365043
Amy Hong <amyhong0502@gmail.com>
2018-12-31 19:27:26 -0800

    feat: add YouTube API

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import YouTubeApi from './../apis/YouTubeApi';
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    console.log('APP' + term);
+  onTermSubmit = async term => {
+    const res = await YouTubeApi.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    console.log(res.data.items);
   };
 
   render() {
