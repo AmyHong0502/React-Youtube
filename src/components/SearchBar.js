3c46f4cd4673be1d207cde8231018666c4593aae
Amy Hong <amyhong0502@gmail.com>
2018-12-31 17:55:51 -0800

    feat: setState search term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
-};
+class SearchBar extends React.Component {
+  state = {
+    term: ''
+  };
+
+  onInputChange = event => {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type='text'
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
