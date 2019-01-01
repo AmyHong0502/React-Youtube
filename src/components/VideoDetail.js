2b8824dec8876d88f25dafa811c3a988aa63e2e5
Amy Hong <amyhong0502@gmail.com>
2018-12-31 21:42:08 -0800

    feat: embed video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,13 @@ const VideoDetail = ({ video }) => {
     return <div />;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className='ui embed'>
+        <iframe title='video_player' src={videoSrc} />
+      </div>
       <div className='ui segment'>
         <h4 className='ui header'>VidelDetail: {video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
